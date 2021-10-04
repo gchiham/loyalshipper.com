5b8327892892292b162d50d2ec10e41c32eb42f8
Gustavo Chi Ham <gchiham@gmail.com>
2021-10-04 17:46:46 -0600

    Update config.js

--- assets/js/modules/config.js
+++ assets/js/modules/config.js
@@ -5,8 +5,8 @@
 export const quoteEndpoint  = 'https://api.batscrm.com/leads';
 export const quoteEndpointES  = 'https://api.batscrm.com/leads-sandbox/sandbox';
 
-export const keyEn = '5ac20785-ad33-4906-b02a-6859cc8da8be';
-export const KeyEs = '8c00130d-872f-4912-81c9-553f38ec61e0';
+export const keyEn = '48ea5f16-ed31-4993-ac2d-f4c60ba7dad6';
+export const KeyEs = '48ea5f16-ed31-4993-ac2d-f4c60ba7dad6';
 
 
 export const carTypeKey = 'Vjcg1k6loyLgR9NGDn6kuiBJUGbIBOFA36iICttpJFaVgyyCxrsrFXCnb4lZ';
