652b64d87adc58ea4316fa3f5fdc5240861d0885
Jose Ordoñez <91579783+jordon3z@users.noreply.github.com>
2021-10-04 21:40:34 -0400

    add file to track analytics

---
+++ assets/js/modules/analyticsTracker.js
@@ -0,0 +1,8 @@
+
+
+const onAnalyticsTracker = () => {
+	console.log('Analytics gtag');
+	gtag('event', 'conversion', {'send_to': '10781490552/aFdCCKbnq_kCEPj6gZUo' });
+}
+
+export default onAnalyticsTracker;
