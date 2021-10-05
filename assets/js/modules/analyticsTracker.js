8b31c5fd434e27a8834b1cda46d01a543e2b52a1
Jose Ordoñez <91579783+jordon3z@users.noreply.github.com>
2021-10-04 21:40:33 -0500

    change to track the url
    change the event convertion to handle the current url

--- assets/js/modules/analyticsTracker.js
+++ assets/js/modules/analyticsTracker.js
@@ -1,8 +1,17 @@
 
 
 const onAnalyticsTracker = () => {
-	console.log('Analytics gtag');
-	gtag('event', 'conversion', {'send_to': '10781490552/aFdCCKbnq_kCEPj6gZUo' });
+  console.log('Analytics gtag');
+  // gtag('event', 'conversion', {'send_to': '10781490552/aFdCCKbnq_kCEPj6gZUo' });
+  var callback = function () {
+    if (typeof(url) != 'undefined') {
+	window.location = url;
+    }};
+	
+    gtag('event', 'conversion', {
+     'send_to': 'AW-10781490552/aFdCCKbnq_kCEPj6gZUo',
+     'event_callback': callback
+    });
 }
 
 export default onAnalyticsTracker;
